d2124332d2da683378a8f3bdda7645f7e3c966f0
Kieran Lewis <kieranlewis1334@gmail.com>
2021-08-23 11:59:12 +1200

    Added functions to add, subtract, multiply and divide

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Hello World');
+
+//calculator functions
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
