8ffff0894a6aa7adde6523d895d8efa31a0015ce
Kieran Lewis <kieranlewis1334@gmail.com>
2021-08-24 10:56:25 +1200

    Added functionality to clear button

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+let currentNumber = 0;
+let displayedNumber = 0;
+let total = 0;
+let currentOperation = '';
+
 //calculator functions
 function add(a,b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 
 function subtract(a,b) {
-    return a - b;
+    return parseInt(a) - parseInt(b);
 }
 
 function multiply(a,b) {
-    return a * b;
+    return parseInt(a) * parseInt(b);
 }
 
 function divide(a,b) {
-    return a / b;
+    return parseInt(a) / parseInt(b);
 }
 
 function operate(a, b, operation) {
@@ -34,7 +39,7 @@ function operate(a, b, operation) {
 
 //DOM Manipulation
 const display = document.querySelector('#display');
-const calcButtons = [...(document.querySelectorAll('#button-grid button'))];
+const calcButtons = [...(document.querySelectorAll('button'))];
 console.log(calcButtons);
 
 calcButtons.forEach(button => button.addEventListener('click', buttonHelper));
@@ -43,20 +48,41 @@ function buttonHelper() {
     const displayArea = document.querySelector('#display p');
     switch (this.textContent) {
         case '+':
+            currentOperation = '+';
+            currentNumber = displayArea.innerHTML;
+            //console.log(`Adding ${number} with ${displayedNumber}`);
+            //total = add(number, displayedNumber);
+            //console.log(`Total: ${total}`);
+            //number = total;
+            displayArea.innerHTML = '';
             break;
         case '-':
+            currentOperation = '-';
+            currentNumber = displayArea.innerHTML;
+            console.log(`Subtracting ${number} with ${displayedNumber}`);
+            total = subtract(number, displayedNumber);
+            console.log(`Total: ${total}`);
+            number = total;
+            displayArea.innerHTML = '';
             break;
         case '*':
             break;
         case '/':
             break;
         case 'C':
+            total = 0;
+            currentNumber = 0;
+            currentOperation = '';
+            displayArea.innerHTML = '';
             break;
         case 'DEL':
             break;
         case '=':
+            console.log(operate(displayedNumber, total, currentOperation));
+            displayArea.innerHTML = operate(displayedNumber, total, currentOperation);
             break;
         default:
             displayArea.innerHTML += this.textContent;
+            displayedNumber = displayArea.innerHTML;
     }
 }
