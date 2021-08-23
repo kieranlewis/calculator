a5f81fb80fc636d7102e6ac3ad28d5ba620e84b9
Kieran Lewis <kieranlewis1334@gmail.com>
2021-08-24 11:53:57 +1200

    Buttons will now let you know what the current operation being used is

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let storedNumber = 0;
 let displayedNumber = 0;
 let storedOperation = '';
 
-let total = 0;
-
 //calculator functions
 function add(a,b) {
     return parseInt(a) + parseInt(b);
@@ -49,33 +47,28 @@ function buttonHelper() {
     const displayArea = document.querySelector('#display p');
     switch (this.textContent) {
         case '+':
+            document.getElementById('btn-plus').classList.add('active');
             storedOperation = '+';
             storedNumber = displayedNumber;
             console.log(`Stored number is ${storedNumber} and stored operation is ${storedOperation}`);
-            //currentNumber = displayedNumber;
-            //console.log(`Adding ${number} with ${displayedNumber}`);
-            //total = add(number, displayedNumber);
-            //console.log(`Total: ${total}`);
-            //number = total;
-            displayArea.innerHTML = '';
             break;
         case '-':
+            document.getElementById('btn-sub').classList.add('active');
             storedOperation = '-';
             storedNumber = displayedNumber;
             console.log(`Stored number is ${storedNumber} and stored operation is ${storedOperation}`);
-            displayArea.innerHTML = '';
             break;
         case '*':
+            document.getElementById('btn-mult').classList.add('active');
             storedOperation = '*';
             storedNumber = displayedNumber;
             console.log(`Stored number is ${storedNumber} and stored operation is ${storedOperation}`);
-            displayArea.innerHTML = '';
             break;
         case '/':
+            document.getElementById('btn-div').classList.add('active');
             storedOperation = '/';
             storedNumber = displayedNumber;
             console.log(`Stored number is ${storedNumber} and stored operation is ${storedOperation}`);
-            displayArea.innerHTML = '';
             break;
         case 'C':
             total = 0;
@@ -86,14 +79,25 @@ function buttonHelper() {
         case 'DEL':
             break;
         case '=':
+            clearActive();
             console.log(`Operating on ${storedNumber} and ${displayedNumber} with ${storedOperation}`);
-            total = operate(storedNumber, displayedNumber, storedOperation);
-            console.log(`New total is ${total}`);
-            displayedNumber = total;
+            displayedNumber = operate(storedNumber, displayedNumber, storedOperation);
             displayArea.innerHTML = displayedNumber;
             break;
         default:
-            displayArea.innerHTML += this.textContent;
-            displayedNumber = displayArea.innerHTML;
+            if(document.getElementsByClassName('active').length != 0) {
+                clearActive();
+                displayArea.innerHTML = this.textContent;
+                displayedNumber = displayArea.innerHTML;        
+            } else {
+                displayArea.innerHTML += this.textContent;
+                displayedNumber = displayArea.innerHTML;  
+            }        
     }
+}
+
+function clearActive() {
+    calcButtons.forEach(button => {
+        button.classList.remove('active');
+    });
 }
