ecb1b74496a9582131874c7e130d27dd150202ef
Kieran Lewis <kieranlewis1334@gmail.com>
2021-08-26 11:39:32 +1200

    Display will now not overflow from the div

--- script.js
+++ script.js
@@ -10,20 +10,20 @@ let previousOperand = '';
 let currentOperation = undefined;
 
 function add(a,b) {
-    return Math.round((parseFloat(a) + parseFloat(b)) * 100) / 100;
+    return Math.round((parseFloat(a) + parseFloat(b)) * 1000) / 1000;
 }
 
 function subtract(a,b) {
-    return Math.round((parseFloat(a) - parseFloat(b)) * 100) / 100;
+    return Math.round((parseFloat(a) - parseFloat(b)) * 1000) / 1000;
 }
 
 function multiply(a,b) {
-    return Math.round((parseFloat(a) * parseFloat(b)) * 100) / 100;
+    return Math.round((parseFloat(a) * parseFloat(b)) * 1000) / 1000;
 }
 
 function divide(a,b) {
     if(b == 0) return 'ERROR';
-    return Math.round((parseFloat(a) / parseFloat(b)) * 100) / 100;
+    return Math.round((parseFloat(a) / parseFloat(b)) * 1000) / 1000;
 }
 
 function operate(a, b, operation) {
